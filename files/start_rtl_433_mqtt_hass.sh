a55f30a6d221a9a24fdbe9bfe0571be125581205
Jonathan Hanson <jonathan@jonathan-hanson.org>
2022-04-18 16:36:20 -0700

    move to pulling the py script from github on build.  Also move the debug flag to the OTHER_ARGS env var

--- files/start_rtl_433_mqtt_hass.sh
+++ files/start_rtl_433_mqtt_hass.sh
@@ -1,1 +1,1 @@
-pipenv run python3 -u ./rtl_433_mqtt_hass.py --debug --host ${MQTT_HOST} --port ${MQTT_PORT} --ca_cert ${MQTT_CA_CERT_PATH} --rtl-topic ${RTL_TOPIC} --discovery-prefix ${DISCOVERY_PREFIX} --interval ${DISCOVERY_INTERVAL} ${OTHER_ARGS}
+pipenv run python3 -u ./rtl_433_mqtt_hass.py --host ${MQTT_HOST} --port ${MQTT_PORT} --ca_cert ${MQTT_CA_CERT_PATH} --rtl-topic ${RTL_TOPIC} --discovery-prefix ${DISCOVERY_PREFIX} --interval ${DISCOVERY_INTERVAL} ${OTHER_ARGS}
