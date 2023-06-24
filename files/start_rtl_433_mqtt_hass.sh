da755b763979c096bcd43c4440c01e9d39fc8ab8
Jonathan Hanson <jonathan@jonathan-hanson.org>
2023-06-24 12:13:53 -0700

    simplify the python package installation.  Fix the docker image tag change

--- files/start_rtl_433_mqtt_hass.sh
+++ files/start_rtl_433_mqtt_hass.sh
@@ -1,1 +1,1 @@
-pipenv run python3 -u ./rtl_433_mqtt_hass.py --host ${MQTT_HOST} --port ${MQTT_PORT} --ca_cert ${MQTT_CA_CERT_PATH} --rtl-topic ${RTL_TOPIC} --discovery-prefix ${DISCOVERY_PREFIX} --device-topic_suffix ${DEVICE_TOPIC_SUFFIX} --interval ${DISCOVERY_INTERVAL} ${OTHER_ARGS}
+python3 -u ./rtl_433_mqtt_hass.py --host ${MQTT_HOST} --port ${MQTT_PORT} --ca_cert ${MQTT_CA_CERT_PATH} --rtl-topic ${RTL_TOPIC} --discovery-prefix ${DISCOVERY_PREFIX} --device-topic_suffix ${DEVICE_TOPIC_SUFFIX} --interval ${DISCOVERY_INTERVAL} ${OTHER_ARGS}
